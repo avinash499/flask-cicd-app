51cf2003eaca604dc7d701c8a5ec1be7826d11ca
avinash99 <avinash49922@gmail.com>
2025-04-11 10:04:02 +0530

    Initial commit - Flask CI/CD app

---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+import unittest
+from app import app
+
+class FlaskAppTest(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_home_page(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
